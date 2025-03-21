4cd83c1f5260c2ac5138c439ad78e1588ee9f03e
Simon Stumpf <simon.stumpf4901@gmail.com>
2025-03-21 21:04:42 +0100

    charakter

---
+++ Zacy_body_2d.gd
@@ -0,0 +1,34 @@
+extends CharacterBody2D
+
+@onready var jumpcount : int = 0
+@onready var speed : float = 200
+@onready var jump : float = -200
+@onready var gravity :float = 800
+@onready var slowout: float = 10
+
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity.y += gravity * delta 
+		
+	if Input.is_action_just_pressed("Jump"):
+		velocity.y = jump
+		jumpcount = +1
+		if jumpcount < 2:
+			velocity.y = jump
+	
+	if is_on_floor():
+		jumpcount = 0
+		
+	if Input.is_action_pressed("Links"):
+		velocity.x = -speed
+	
+	else:
+		velocity.x = move_toward(velocity.x, 0, slowout)
+		
+	if Input.is_action_pressed("Rechts"):
+		velocity.x = speed
+		
+	else:
+		velocity.x = move_toward(velocity.x, 0, slowout)
+		
+	move_and_slide()
--- start_button.gd
+++ start_button.gd
@@ -1,6 +1,8 @@
 extends Button
 
+var main_scene_path = "res://scenes/game/main_scene.tscn"
+
 
 func _on_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/game/main_scene.tscn")
+	get_tree().call_deferred("change_scene_to_file", main_scene_path)
 	

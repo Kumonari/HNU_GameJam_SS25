c4864610c6d09485821a73104b8272f53c8dc166
Kumonari <184982588+Kumonari@users.noreply.github.com>
2025-03-22 14:52:19 +0100

    hook v1

--- scripts/main_character.gd
+++ scripts/main_character.gd
@@ -14,7 +14,9 @@ var slowout: float = 100
 var hook_pos: Vector2
 var hooked: bool = false
 var arm_lenght: float = 500 # should equal $Hook.raycast1.target_position_x
-var current_arm_lenght: float 
+var current_arm_lenght: float
+var swing_speed: float = 0.6
+var hook_speed: float = 1500
 
 var health = 3
 
@@ -23,19 +25,34 @@ func _ready() -> void:
 	current_arm_lenght = arm_lenght
 
 
-
-
 func _physics_process(delta: float) -> void:
 	# Base Controls
 	gravity(delta)
 	movement(delta)
 	
-	#hook(delta)
-	
-	
-	
+	# Hook logic and visuals
+	hook()
+	update()
+	if hooked:
+		gravity(delta)
+		swing(delta)
+		velocity *= swing_speed
+		
 	move_and_slide()
 
+
+func _draw() -> void:
+	var pos = global_position
+	
+	if hooked:
+		draw_line(Vector2(0,-16), to_local(hook_pos), Color(0.35, 0.7, 0.9), 3, true)
+	else:
+		return
+		
+		var colliding = hook_node.is_colliding()
+		var collide_point = hook_node.get_collision_point()
+		if colliding and pos.distance_to(collide_point) < arm_lenght:
+			draw_line(Vector2(0, -16), to_local(collide_point), Color(1, 1, 1, 0.25), 0.5, true)
 
 func gravity(delta) -> void:
 	if not is_on_floor():
@@ -59,11 +76,38 @@ func movement(delta) -> void:
 		velocity.x = move_toward(velocity.x, 0, slowout * delta)
 
 
-#func hook(delta) -> void:
-	#if hook_node:
-		#hook_node.look_at(get_global_mouse_position())
-		#if Input.is_action_pressed("l_click"):
-			#hook_pos = get_hook_pos()
-			#if hook_pos:
-				#hooked = true
-				#current_arm_lenght = global_position.distance_to(hook_pos)
+func hook() -> void:
+	if hook_node:
+		hook_node.look_at(get_global_mouse_position())
+		if Input.is_action_pressed("l_click"):
+			hook_pos = get_hook_pos()
+			if not hook_pos == Vector2.ZERO:
+				hooked = true
+				current_arm_lenght = global_position.distance_to(hook_pos)
+		if Input.is_action_just_released("l_click") and hooked:
+			hooked = false
+
+
+func get_hook_pos() -> Vector2:
+	for raycast in hook_node.get_children():
+		if raycast.is_colliding():
+			return raycast.get_collision_point()
+	
+	return Vector2.ZERO
+
+
+func update() -> void:
+	pass
+
+
+func swing(delta) ->void:
+	var radius = global_position - hook_pos
+	if velocity.length() < 0.01 or radius.length() < 10: return
+	var angle = acos(radius.dot(velocity)) / (radius.length() * velocity.length())
+	var rad_vel = cos(angle) * velocity.length()
+	velocity += radius.normalized() * -rad_vel
+	
+	if global_position.distance_to(hook_pos) > current_arm_lenght:
+		global_position = hook_pos + radius.normalized() * current_arm_lenght
+		
+	velocity += (hook_pos - global_position).normalized() * hook_speed * delta

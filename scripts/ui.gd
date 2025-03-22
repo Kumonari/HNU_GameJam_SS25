92ef3c1678ee1eb261c1e83dacda00c64d7a1233
Kumonari <184982588+Kumonari@users.noreply.github.com>
2025-03-22 22:58:11 +0100

    cleanup and heathbar ui

--- scripts/ui.gd
+++ scripts/ui.gd
@@ -2,21 +2,30 @@ extends Control
 
 @onready var Health1 = $Leben1
 @onready var Health2 = $Leben2
-@onready var Health3 = $Leben3  
+@onready var Health3 = $Leben3
+@onready var player: MainCharacter = $"../CharacterBody2D"
 
-func _ready() -> void:
+
+func _process(delta):
 	Health_Screen()
 
 func Health_Screen():
 	if player.health == 3:
-		Health1.visible = true
-		Health2.visible = true
-		Health3.visible = true
+		Health1.frame = 0
+		Health2.frame = 0
+		Health3.frame = 0
 	if player.health == 2:
-		Health1.visible = true
-		Health2.visible = true
-		Health3.visible = false
+		Health1.frame = 0
+		Health2.frame = 0
+		Health3.frame = 1
 	if player.health == 1:
-		Health1.visible = true
-		Health2.visible = false
-		Health3.visible = false
+		Health1.frame = 0
+		Health2.frame = 1
+		Health3.frame = 1
+	if player.health == 0:
+		Health1.frame = 1
+		Health2.frame = 1
+		Health3.frame = 1
+		
+		await get_tree().create_timer(1.0).timeout
+		get_tree().quit()
